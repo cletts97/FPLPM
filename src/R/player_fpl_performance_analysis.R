76e9c868bd5a6715343433075f20abde95a55dd0
cletts97 <c.letts97@outlook.com>
2025-03-06 17:39:49 +0000

    Begin analysis. Calculating and ranking players points per 90.

---
+++ src/R/player_fpl_performance_analysis.R
@@ -0,0 +1,12 @@
+library(dplyr)
+
+players <- readRDS("data/processed/players_clean.rds")
+
+players_small <- players %>%
+        select(web_name, player_id, team_id, total_points, minutes_played, player_type) %>%
+        filter(as.integer(player_type) != 5 & as.integer(minutes_played) > 250) %>%
+        mutate(
+                total_points = ifelse(is.na(total_points), 0, total_points),
+                minutes_played = ifelse(is.na(minutes_played), 0, minutes_played)) %>%
+        mutate(points_per_90 = if_else(as.integer(minutes_played) > 0, (total_points / minutes_played) * 90, 0)) %>%
+        arrange(desc(points_per_90))
