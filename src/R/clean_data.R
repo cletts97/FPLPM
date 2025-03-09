a7bdf6724faba8db0c083f74ecd4a41ad72b47de
cletts97 <c.letts97@outlook.com>
2025-03-09 15:31:56 +0000

    Type casting all columns in each dataframe to the correct data type.
    Modified the performance analysis script as type-casting in the script is no longer required.

--- src/R/clean_data.R
+++ src/R/clean_data.R
@@ -1,5 +1,6 @@
 library(dplyr)
 
+## Load in raw data
 chips_data <- readRDS("data/raw/chips_data.rds")
 events <- readRDS("data/raw/events.rds")
 fixtures <- readRDS("data/raw/fixtures.rds")
@@ -12,30 +13,85 @@ players <- readRDS("data/raw/players.rds")
 teams <- readRDS("data/raw/teams.rds")
 total_players <- readRDS("data/raw/total_players.rds")
 
+## Clean chips_data
 chips_data_clean <- chips_data %>%
         select(-overrides.pick_multiplier,
-                           chips_id = id,
-                           start_gw = start_event, 
-                           stop_gw = stop_event, 
-                           number_of_chips = number, 
-                           element_types_override = overrides.element_types, 
-                           squad_size_rules_override = overrides.rules.squad_squadsize) %>%
+               chips_id = id,
+               start_gw = start_event,
+               stop_gw = stop_event,
+               number_of_chips = number,
+               element_types_override = overrides.element_types,
+               squad_size_rules_override = overrides.rules.squad_squadsize) %>%
+        mutate(
+                across(
+                        c(
+                                chips_id,
+                                number_of_chips,
+                                start_gw,
+                                stop_gw,
+                                squad_size_rules_override
+                        ),
+                        as.integer
+                ),
+                across(c(name, chip_type), as.factor),
+                across(c(element_types_override), as.list)
+               ) %>%
         arrange(chips_id)
 
 saveRDS(chips_data_clean, "data/processed/chips_data_clean.rds")
 
 
+## Clean events
 events_clean <- events %>%
         select(-c(overrides.pick_multiplier, overrides.element_types, released, deadline_time_game_offset, release_time),
                gw_id = id,
                top_player = top_element,
                top_player_id = top_element_info.id,
                top_player_points = top_element_info.points) %>%
+        mutate(
+                across(
+                        c(
+                                gw_id,
+                                average_entry_score,
+                                highest_scoring_entry,
+                                deadline_time_epoch,
+                                highest_score,
+                                ranked_count,
+                                most_selected,
+                                most_transferred_in,
+                                top_player,
+                                transfers_made,
+                                most_captained,
+                                most_vice_captained,
+                                top_player_id,
+                                top_player_points
+                        ), 
+                        as.integer
+                ),
+                across(
+                        c(
+                                finished,
+                                data_checked,
+                                is_previous,
+                                is_current,
+                                is_next,
+                                cup_leagues_created,
+                                h2h_ko_matches_created,
+                                can_enter,
+                                can_manage
+                        ),
+                        as.logical
+                ),
+                across(name, as.character),
+                across(deadline_time, ~ as.POSIXct(., format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")),
+                across(chip_plays, as.list)
+        ) %>%
         arrange(gw_id)
 
 saveRDS(events_clean, "data/processed/events_clean.rds")
 
 
+## Clean fixtures
 fixtures_clean <- fixtures %>%
         select(c(-provisional_start_time, minutes),
                gw_id = event,
@@ -47,34 +103,110 @@ fixtures_clean <- fixtures %>%
                home_team_difficulty = team_h_difficulty,
                away_team_difficulty = team_a_difficulty,
                pl_fixture_id = pulse_id) %>%
+        mutate(
+                across(
+                        c(
+                                code,
+                                gw_id,
+                                fixture_id,
+                                minutes,
+                                away_team,
+                                away_team_score,
+                                home_team,
+                                home_team_score,
+                                home_team_difficulty,
+                                away_team_difficulty,
+                                pl_fixture_id
+                        ),
+                        as.integer
+                ),
+                across(
+                        c(
+                                finished,
+                                finished_provisional,
+                                started
+                        ),
+                        as.logical
+                ),
+                across(kickoff_time, ~ as.POSIXct(., format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")),
+                across(stats, as.list)
+        ) %>%
         arrange(gw_id, fixture_id)
 
 saveRDS(fixtures_clean, "data/processed/fixtures_clean.rds")
 
 
+##Clean phases
 phases_clean <- phases %>%
         rename(start_gw = start_event,
                stop_gw = stop_event,
                phase_id = id) %>%
+        mutate(
+                across(
+                        c(
+                                phase_id,
+                                start_gw,
+                                stop_gw,
+                                highest_score
+                        )
+                ),
+                across(name, as.factor)
+        ) %>%
         arrange(phase_id)
 
 saveRDS(phases_clean, "data/processed/phases_clean.rds")
 
 
+## Clean player_types
 player_types_clean <- player_types %>%
         select(-c(plural_name_short, squad_min_select, squad_max_select),
                   position_id = id,
                   player_count = element_count,
                   squad_min_players = squad_min_play,
                   squad_max_players = squad_max_play) %>%
+        mutate(
+                across(
+                        c(
+                                position_id,
+                                squad_min_players,
+                                squad_max_players,
+                                squad_select,
+                                sub_positions_locked,
+                                player_count
+                        ),
+                        as.integer
+                ),
+                across(
+                        c(
+                                plural_name, 
+                                singular_name, 
+                                singular_name_short
+                        ),
+                        as.factor
+                ),
+                across(ui_shirt_specific, as.logical)
+        ) %>%
         arrange(position_id)
 
 saveRDS(player_types_clean, "data/processed/player_types_clean.rds")
 
 
+## Clean players
 players_clean <- players %>%
         filter(can_select == TRUE) %>%
-        select(second_name, first_name, id, team, total_points, everything(), -c(can_transact, can_select, photo, removed, special, squad_number),
+        select(
+                second_name, 
+                first_name, id, 
+                team, total_points, 
+                everything(), 
+                -c(
+                        can_transact, 
+                        can_select, 
+                        photo, 
+                        removed, 
+                        special, 
+                        squad_number
+                ),
                player_code = code,
                player_type = element_type,
                exp_points_next_gw = ep_next,
@@ -84,23 +216,93 @@ players_clean <- players %>%
                transfers_in_gw = transfers_in_event,
                transfers_out_gw = transfers_out_event,
                minutes_played = minutes,
-               team_id = team) %>%
+               team_id = team
+               ) %>%
+        mutate(
+                across(
+                        c(
+                                player_id:player_type,
+                                gw_points,
+                                now_cost,
+                                team_code:transfers_out_gw,
+                                region,
+                                minutes_played:bonus
+                        ),
+                        as.integer
+                ),
+                across(
+                        c(
+                                exp_points_next_gw,
+                                exp_points_this_gw,
+                                form,
+                                points_per_game,
+                                selected_by_percent,
+                                value_form,
+                                value_season
+                        ),
+                        as.numeric
+                ),
+                across(
+                        c(
+                                second_name,
+                                first_name,
+                                news,
+                                web_name,
+                                opta_code
+                        ),
+                        as.character
+                ),
+                across(
+                        c(
+                                in_dreamteam,
+                                has_temporary_code
+                        ),
+                        as.logical
+                ),
+                across(news_added, ~ as.POSIXct(., format = "%Y-%m-%dT%H:%M:%SZ", tz = "UTC")),
+                status = factor(status, levels = c("a", "i", "u", "d", "n"))
+        ) %>%
         arrange(desc(total_points), second_name)
 
 saveRDS(players_clean, "data/processed/players_clean.rds")
 
 
+## Clean teams
 teams_clean <- teams %>%
         select(id, name, position, strength, everything(), -c(team_division, unavailable),
                team_code = code,
                team_id = id,
                pl_team_id = pulse_id) %>%
+        mutate(
+                across(
+                        c(
+                                team_id,
+                                position:draw,
+                                loss:points,
+                                win:pl_team_id
+                        ),
+                        as.integer
+                ),
+                across(
+                        c(
+                                name,
+                                short_name
+                        ),
+                        as.factor
+                ),
+                across(form, as.numeric)
+        ) %>%
         arrange(team_id)
 
 saveRDS(teams_clean, "data/processed/teams_clean.rds")
 
+## Clean player_stats_labels
+player_stats_labels_clean <- player_stats_labels %>% mutate(across(1:2, as.character))
+saveRDS(player_stats_labels_clean, "data/processed/player_stats_labels_clean.rds")
+
+
+total_players <- as.integer(total_players)
+saveRDS(total_players, "data/processed/total_players_clean.rds")
 
 saveRDS(game_config, "data/processed/game_config_clean.rds")
 saveRDS(game_settings, "data/processed/game_settings_clean.rds")
-saveRDS(player_stats_labels, "data/processed/player_stats_labels_clean.rds")
-saveRDS(total_players, "data/processed/total_players_clean.rds")
--- src/R/player_fpl_performance_analysis.R
+++ src/R/player_fpl_performance_analysis.R
@@ -2,11 +2,13 @@ library(dplyr)
 
 players <- readRDS("data/processed/players_clean.rds")
 
-players_small <- players %>%
+players_points_per_90 <- players %>%
         select(web_name, now_cost, total_points, minutes_played, player_type, player_id, team_id) %>%
-        filter(as.integer(player_type) != 5 & as.integer(minutes_played) > 300) %>%
+        filter(player_type != 5 & minutes_played > 300) %>%
         mutate(
                 total_points = ifelse(is.na(total_points), 0, total_points),
                 minutes_played = ifelse(is.na(minutes_played), 0, minutes_played)) %>%
-        mutate(points_per_90 = if_else(as.integer(minutes_played) > 0, (total_points / minutes_played) * 90, 0)) %>%
+        mutate(points_per_90 = if_else(minutes_played > 0, (total_points / minutes_played) * 90, 0)) %>%
         arrange(desc(points_per_90))
+
+saveRDS(players_points_per_90, "reports/performance_analysis/players_points_per_90.rds")
